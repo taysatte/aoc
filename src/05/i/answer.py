efc8f463ebb799d77f0c7c094468f9bacf21aaf5
taysatte <taylor.sattenfield@gmail.com>
2024-11-30 21:01:12 -0600

    idk committing from last year

---
+++ src/05/i/answer.py
@@ -0,0 +1,15 @@
+import sys
+
+
+class Solution():
+
+    def run(self) -> None:
+        self.main()
+
+    def main(self) -> None:
+        print("Main Method")
+
+
+if __name__ == "__main__":
+
+    s = Solution().run()
